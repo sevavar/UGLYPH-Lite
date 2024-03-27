b975e4c1e7978da637bf7bdf78849fd038a244fa
sevavar <145381325+sevavar@users.noreply.github.com>
2024-03-28 00:54:51 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,8 +13,8 @@ let guiTextColor = '#bababa';
 
 // Blob
 let points = []; // Array to store blob vertex points
-let amount = 3000; // Number of blob points
-let blobSize = 600; // Size of the blob
+let amount = 1000; // Number of blob points
+let blobSize = 200; // Size of the blob
 let noiseScale = 0.004;
 let smoothingEnabled = true;
 
